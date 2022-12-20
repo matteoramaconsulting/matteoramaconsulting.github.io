62a32a53e68f96c19ee4c56b66f7b24966de4a12
ramaconsultingsrl <matteo.peluso@ramaconsulting.it>
2022-12-20 14:55:42 +0100

    1.0.0 Primo Test di Sito

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 /*!
-* Start Bootstrap - Agency v7.0.11 (https://startbootstrap.com/theme/agency)
+* Start Bootstrap - Rama Consulting v7.0.11 (https://startbootstrap.com/theme/agency)
 * Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
+* Licensed under MIT (https://github.com/StartBootstrap/Agency/blob/master/LICENSE)
 */
 //
 // Scripts
